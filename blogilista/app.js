20498d74a0794592d51d1526daaa58218166e494
pekko <pekko.pulliainen@outlook.com>
2026-01-05 23:20:58 +0200

    4.15

--- blogilista/app.js
+++ blogilista/app.js
@@ -1,11 +1,14 @@
-const config = require("./utils/config");
-const express = require("express");
-const app = express();
 const cors = require("cors");
-const blogsRouter = require("./controllers/blogs");
-const middleware = require("./utils/middleware");
-const logger = require("./utils/logger");
+const express = require("express");
 const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const loginRouter = require("./controllers/login");
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+
+const app = express();
 
 mongoose.set("strictQuery", false);
 
@@ -25,7 +28,9 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- blogilista/models/blog.js
+++ blogilista/models/blog.js
@@ -1,10 +1,23 @@
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+  },
+  url: {
+    type: String,
+  },
+  likes: {
+    type: Number,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 blogSchema.set("toJSON", {
